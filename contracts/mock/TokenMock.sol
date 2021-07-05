5e112096fe5f5f6c2c59b7c22864b63e7d17e3fa
Dmitriy Darzhain <dadarzhain@avito.ru>
2021-07-05 13:14:44 +0300

    update version

--- contracts/mock/TokenMock.sol
+++ contracts/mock/TokenMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/access/Ownable.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/ERC20.sol";
